6ab172688875eab66098228562cb1ed37a6f4262
zsbati <zbati123@gmail.com>
2025-05-21 19:04:53 +0100

    all glitches fixed, testing complete.

--- users/urls.py
+++ users/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
     path('user/<int:user_id>/update/', views.update_user, name='update_user'),
     path('user/<int:user_id>/delete/', views.delete_user, name='delete_user'),
     path('password/change/', auth_views.PasswordChangeView.as_view(
-        template_name='agenda/registration/password_change.html',
+        template_name='password_change.html',
         success_url='/users/password/change/done/'
     ), name='change_password'),
     path('password/change/done/', auth_views.PasswordChangeDoneView.as_view(
-        template_name='agenda/registration/password_change_done.html'
+        template_name='password_change_done.html'
     ), name='password_change_done'),
     path('user/<int:user_id>/password/', views.change_user_password, name='change_user_password'),
     path('logout/', auth_views.LogoutView.as_view(next_page='agenda:dashboard'), name='logout'),
